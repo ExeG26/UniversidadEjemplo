4a3898ec13f43ba7d42a0bfbecafa746ab09d807
Fedee-Mauro <federicolmauro@gmail.com>
2023-09-08 13:01:11 -0300

    Create InscripcionData.java
    Se realiza parte de la clase InscrpcionData

---
+++ src/universidadejemplo/AccesoADatos/InscripcionData.java
@@ -0,0 +1,17 @@
+package universidadejemplo.AccesoADatos;
+
+import java.sql.*;
+
+public class InscripcionData {
+    
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+
+    public InscripcionData() {
+    }
+    
+    public void obtenerMateriasCursadas(){
+        
+    }
+}
