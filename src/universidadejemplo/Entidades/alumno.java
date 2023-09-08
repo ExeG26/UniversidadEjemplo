38aab507f225b4e0a6c8f88535fb05d9cd057704
Fedee-Mauro <federicolmauro@gmail.com>
2023-09-08 11:41:17 -0300

    Update alumno.java
    Se realizó la clase Alumno con atributos correspondientes.

--- src/universidadejemplo/Entidades/alumno.java
+++ src/universidadejemplo/Entidades/alumno.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadejemplo.Entidades;
 
-/**
- *
- * @author ignac
- */
+import java.sql.Date;
+
 public class alumno {
+    
+    private int dni;
+    private String apellido, nombre;
+    private Date fechaNacimiento;
+    private boolean estado;
+
+    public alumno() {
+    }
+
+    public alumno(int dni, String apellido, String nombre, Date fechaNacimiento, boolean estado) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estado = estado;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
     
 }
